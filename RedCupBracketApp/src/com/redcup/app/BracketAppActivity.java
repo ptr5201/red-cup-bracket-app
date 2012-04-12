c09991769491f4b0a7ae146c3bd8dea18025c5c5
ptr5201 <ptr5201@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-04-12 18:01:04 +0000

    Added main menu buttons and logging functionality (temporarily) to be executed when a main menu button (RelativeLayout) is clicked

--- RedCupBracketApp/src/com/redcup/app/BracketAppActivity.java
+++ RedCupBracketApp/src/com/redcup/app/BracketAppActivity.java
@@ -2,12 +2,39 @@ package com.redcup.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class BracketAppActivity extends Activity {
+	
+	private static final String TAG = "BracketAppActivity";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    	Log.v(TAG, "we are up and running");
+    }
+    
+    public void switchActivityScreen(View v) {
+    	Log.v(TAG, "something clicked: " + v.getId());
+    	switch(v.getId()) {
+    	case R.id.createTournament:
+    		Log.v(TAG, getString(R.string.createTournament));
+    		break;
+    	case R.id.loadTournament:
+    		Log.v(TAG, getString(R.string.loadTournament));
+    		break;
+    	case R.id.pastTournaments:
+    		Log.v(TAG, getString(R.string.pastTournaments));
+    		break;
+    	case R.id.participants:
+    		Log.v(TAG, getString(R.string.participants));
+    		break;
+    	case R.id.help:
+    		Log.v(TAG, getString(R.string.help));
+    		break;
+    	}
     }
 }
