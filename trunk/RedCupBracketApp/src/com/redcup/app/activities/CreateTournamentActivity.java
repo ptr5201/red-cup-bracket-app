1bf2332d3ac3ba2e8dbd529520482836daf4c8bf
ptr5201 <ptr5201@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-04-21 04:54:51 +0000

    Updated Create Tournament screen to set up a Tournament object when the "Create Tournament" button is clicked. Also added TODO notes about how to proceed with rest of Tournament creation and setup.

--- trunk/RedCupBracketApp/src/com/redcup/app/activities/CreateTournamentActivity.java
+++ trunk/RedCupBracketApp/src/com/redcup/app/activities/CreateTournamentActivity.java
@@ -1,17 +1,78 @@
 package com.redcup.app.activities;
 
-import com.redcup.app.R;
-import com.redcup.app.R.layout;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.redcup.app.R;
+import com.redcup.app.model.BracketTypeEnum;
+import com.redcup.app.model.SingleEliminationBracketStrategy;
+import com.redcup.app.model.Tournament;
 
 public class CreateTournamentActivity extends Activity {
+	
+	private static final String TAG = "CreateTournamentActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.createtournament);
+		
+		Spinner bracketType = (Spinner) findViewById(R.id.bracketType);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.bracket_choices, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		bracketType.setAdapter(adapter);
+	}
+	
+	public void createTournament(View v) {
+		EditText nameField = (EditText) findViewById(R.id.nameField);
+		Spinner bracketType = (Spinner) findViewById(R.id.bracketType);
+		EditText participantCountField = (EditText) findViewById(R.id.participantCountField);
+		
+		// TODO: enable the "Create Tournament" button iff the 
+		// name field AND bracket type are not empty
+		if (nameField.getText() == null || 
+				nameField.getText().toString().equals("")) {
+			Log.e(TAG, "Tournament Name must have a non-empty value");
+			return;
+		}
+		
+		if (bracketType.getSelectedItemId() == AdapterView.INVALID_ROW_ID) {
+			Log.e(TAG, "Bracket Type must have a non-empty value");
+			return;
+		}
+		BracketTypeEnum bracketTypes[] = BracketTypeEnum.values();
+		BracketTypeEnum tournamentBracketType = bracketTypes[(int) bracketType.getSelectedItemId()];
+		
+		Tournament t = new Tournament();
+		t.setName(nameField.getText().toString());
+		
+		//TODO: determine how to set bracket strategy based on bracket type
+		switch (tournamentBracketType) {
+		case SINGLE_ELIMINATION:
+			//t.setStrategy(new SingleEliminationBracketStrategy(null)); --> ambiguous constructor
+			break;
+		case DOUBLE_ELIMINATION:
+			//
+			break;
+		}
+		
+		//TODO: do we want a bracket type?
+		//t.setBracketType(tournamentBracketType);
+		if (participantCountField.getText() != null &&
+				participantCountField.getText().toString() != null) {
+			//TODO: set max participant capacity?
+			//t.setParticipantCapacity(Integer.parseInt(participantCountField.getText().toString()));
+		}
+		
+		// TODO: we have the tournament set up, now we need to persist it
+		Log.v(TAG, "Tournament created. Persist tournament and proceed to Tournament View");
 	}
 
 }
