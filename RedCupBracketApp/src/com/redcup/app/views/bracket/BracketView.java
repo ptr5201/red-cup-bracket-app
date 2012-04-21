1679d2fa86d3bc71422fb4183dab72f34205a9e7
jal2633@g.rit.edu <jal2633@g.rit.edu@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-04-21 20:42:48 +0000

    - Began work on the BracketView component

---
+++ RedCupBracketApp/src/com/redcup/app/views/bracket/BracketView.java
@@ -0,0 +1,63 @@
+package com.redcup.app.views.bracket;
+
+import com.redcup.app.views.bracket.layouts.BracketViewLayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class BracketView extends ViewGroup {
+	// The layout algorithm to use
+	private BracketViewLayout layout = null;
+
+	public BracketView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public BracketView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public BracketView(Context context) {
+		super(context);
+	}
+
+	/**
+	 * Sets the {@code BracketViewLayout} used to layout components in this
+	 * {@code BracketView}.
+	 * 
+	 * @param layout
+	 *            the {@code BracketViewLayout} used to layout components in
+	 *            this {@code BracketView}.
+	 */
+	public void setLayoutAlgorithm(BracketViewLayout layout) {
+		this.layout = layout;
+		this.layout.setContext(this);
+		this.invalidate();
+	}
+
+	/**
+	 * Returns the {@code BracketViewLayout} used to layout components in this
+	 * {@code BracketView}.
+	 * 
+	 * @return the {@code BracketViewLayout} used to layout components in this
+	 *         {@code BracketView}.
+	 */
+	public BracketViewLayout getLayoutAlgorithm() {
+		return this.layout;
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		if (this.layout != null) {
+			this.layout.onLayout(changed, l, t, r, b);
+		}
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		if (this.layout != null) {
+			
+		}
+	}
+}
