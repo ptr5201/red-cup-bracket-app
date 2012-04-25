17790878df82f1f096775cd76513e992d6a7e5b5
jal2633@g.rit.edu <jal2633@g.rit.edu@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-04-25 06:07:02 +0000

    - Added functionality to the BracketViewSlot control - Attempts at theming buttons

--- trunk/RedCupBracketApp/src/com/redcup/app/views/bracket/BracketViewSlot.java
+++ trunk/RedCupBracketApp/src/com/redcup/app/views/bracket/BracketViewSlot.java
@@ -1,14 +1,21 @@
 package com.redcup.app.views.bracket;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
-import android.graphics.RectF;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PaintDrawable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class BracketViewSlot extends View {
+import com.redcup.app.views.themes.FlatButtonTheme;
+
+public class BracketViewSlot extends ViewGroup {
+
+	private BracketSlotButton slotButton;
+	private Button removeButton;
+	private PaintDrawable background = new PaintDrawable(Color.LTGRAY);
 
 	private int collapsedHeight = 60;
 	private int collapsedWidth = 240;
@@ -16,64 +23,58 @@ public class BracketViewSlot extends View {
 	private int expandedHeight = 120;
 	private int expandedWidth = 320;
 
-	int r = 255;
-	int b = 0;
-	int g = 0;
-	boolean hasFocus = false;
-
 	public BracketViewSlot(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		this.initialize();
+		this.initialize(context);
 	}
 
 	public BracketViewSlot(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		this.initialize();
+		this.initialize(context);
 	}
 
 	public BracketViewSlot(Context context) {
 		super(context);
-		this.initialize();
+		this.initialize(context);
 	}
 
-	private void initialize() {
-		this.setOnClickListener(new OnClickListener() {
+	private void initialize(Context context) {
+		// Create the slot expansion/editing button
+		this.slotButton = new BracketSlotButton(context);
+		this.slotButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				r = 0;
-				g = 255;
-				b = 0;
-				invalidate();
-			}
-		});
-
-		this.setOnLongClickListener(new OnLongClickListener() {
-
-			@Override
-			public boolean onLongClick(View v) {
-				r = 0;
-				g = 0;
-				b = 255;
-				invalidate();
-				return true;
-			}
-		});
-
-		this.setOnFocusChangeListener(new OnFocusChangeListener() {
-
-			@Override
-			public void onFocusChange(View v, boolean hasFocus) {
-				BracketViewSlot.this.hasFocus = hasFocus;
+				v.setSelected(true);
+				BracketViewSlot.this.removeButton.setVisibility(VISIBLE);
+				BracketViewSlot.this.background.setAlpha(255);
 				BracketViewSlot.this.invalidate();
 			}
 		});
+		this.addView(slotButton);
+
+		// Create the "remove" button
+		this.removeButton = new Button(context);
+		this.removeButton.setText("X");
+		this.removeButton.setVisibility(INVISIBLE);
+		this.addView(this.removeButton);
+
+		// Background drawable
+		this.background.setBounds(0, 0, this.expandedWidth,
+				this.collapsedHeight);
+		this.background.setCornerRadius(20);
+		this.background.setAlpha(0);
+		this.setBackgroundDrawable(this.background);
 	}
 
 	public void reset() {
-		r = 255;
-		g = 0;
-		b = 0;
+		if (this.slotButton != null) {
+			this.slotButton.reset();
+		}
+		if (this.removeButton != null) {
+			this.removeButton.setVisibility(INVISIBLE);
+			this.background.setAlpha(0);
+		}
 	}
 
 	public void setCollapsedWidth(int width) {
@@ -113,49 +114,9 @@ public class BracketViewSlot extends View {
 	}
 
 	@Override
-	protected void onDraw(Canvas canvas) {
-		Paint paint = new Paint();
-		paint.setAntiAlias(true);
-		paint.setStrokeWidth(4);
-
-		// Draw expanded area
-		paint.setARGB(127, r, g, b);
-		paint.setStyle(Style.FILL);
-		canvas.drawRoundRect(new RectF(0, 0, this.expandedWidth,
-				this.expandedHeight), 20, 20, paint);
-
-		// Draw border
-		int borderShade = this.hasFocus ? 255 : 0;
-		paint.setARGB(255, borderShade, borderShade, 0);
-		paint.setStyle(Style.FILL);
-		canvas.drawRoundRect(new RectF(0, 0, this.collapsedWidth,
-				this.collapsedHeight), 20, 20, paint);
-
-		// Draw background
-		paint.setARGB(255, r, g, b);
-		paint.setStyle(Style.FILL);
-		canvas.drawRoundRect(new RectF(4, 4, this.collapsedWidth - 4,
-				this.collapsedHeight - 4), 16, 16, paint);
-
-		// Draw label
-		canvas.save();
-		canvas.clipRect(new RectF(4, 4, this.collapsedWidth - 4,
-				this.collapsedHeight - 4));
-		paint.setARGB(255, 0, 0, 0);
-		paint.setTextSize(32);
-		paint.setUnderlineText(true);
-		canvas.drawText("Text Goes Here and Stuff", 10, 40, paint);
-		canvas.restore();
-	}
-
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		// int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-		// int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-		// int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-		// int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-		//
-		// this.setMeasuredDimension(widthSize, heightSize);
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		this.slotButton.layout(0, 0, this.collapsedWidth, this.collapsedHeight);
+		this.removeButton.layout(this.collapsedWidth + 5, 5,
+				this.expandedWidth - 5, this.collapsedHeight - 5);
 	}
 }
