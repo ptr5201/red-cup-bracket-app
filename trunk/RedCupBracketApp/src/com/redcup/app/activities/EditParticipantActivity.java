59ca1adbe4bacce74dedf7e986cc81bf21d659a0
ajk8409@g.rit.edu <ajk8409@g.rit.edu@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-04-25 22:50:41 +0000

    Fixed save button problem in edit participant and made participant manager consistent with participant selector

--- trunk/RedCupBracketApp/src/com/redcup/app/activities/EditParticipantActivity.java
+++ trunk/RedCupBracketApp/src/com/redcup/app/activities/EditParticipantActivity.java
@@ -3,8 +3,11 @@ package com.redcup.app.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.redcup.app.R;
@@ -15,18 +18,42 @@ public class EditParticipantActivity extends Activity{
 	private static final String TAG = "EditParticipantActivity";
 	private RedCupDB db;
 	private EditText nameField;
+	private Button saveButton;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.editparticipantmenu);
+		saveButton = (Button) findViewById(R.id.saveButton);
+		saveButton.setEnabled(false);
 		nameField = (EditText) findViewById(R.id.editNameEditText);
 		nameField.setText(this.getIntent().getStringExtra("name"));
+		nameField.addTextChangedListener(new TextWatcher(){
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				if (nameField.getText().toString().equals(getIntent().getStringExtra("name"))){
+					saveButton.setEnabled(false);
+				}
+				else{
+					saveButton.setEnabled(true);
+				}
+			}
+		});
 		db = new RedCupDB(this);
 	}
 	
 	public void editParticipant(View v){
-		//EditText nameField = (EditText) findViewById(R.id.editNameEditText);
-		
+	
 		if (nameField.getText() == null || 
 				nameField.getText().toString().equals("")) {
 			Log.e(TAG, "Participant must have a name");
@@ -35,15 +62,10 @@ public class EditParticipantActivity extends Activity{
 		else{
 			String oldName = this.getIntent().getStringExtra("name");
 			String newName = nameField.getText().toString();
-			// TODO: change to a result activity
 			
 			db.open();
 			db.editParticipant(newName, oldName);
 			db.close();
-			
-			Log.v(TAG, getString(R.string.participants));
-			Intent participantManager = new Intent(this, ParticipantManagerActivity.class);
-			startActivity(participantManager);
 		}
 		
 		
