3303dea8abf7506b6782284875974c6fcf6c5e44
matthew.j.alioto@gmail.com <matthew.j.alioto@gmail.com@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-05-14 10:01:00 +0000

    Added some more to loadtournament (intent, listener)

--- trunk/RedCupBracketApp/src/com/redcup/app/activities/LoadTournamentActivity.java
+++ trunk/RedCupBracketApp/src/com/redcup/app/activities/LoadTournamentActivity.java
@@ -3,15 +3,18 @@ package com.redcup.app.activities;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.redcup.app.R;
 import com.redcup.app.data.RedCupDB;
 import com.redcup.app.model.Tournament;
+import com.redcup.app.model.TournamentManager;
 
 public class LoadTournamentActivity extends ListActivity {
 
@@ -29,12 +32,26 @@ public class LoadTournamentActivity extends ListActivity {
 		ListView tListView = (ListView)findViewById(android.R.id.list);
 		
 //		setListAdapter(new ArrayAdapter<String>(this, R.id.tournamentListItemId, tournaments));
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, android.R.id.text1, tournaments);
+		
+		tListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		    @Override
+		    public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+		    	// We can't save tournaments yet, so...
+//		    	Intent loadTournament = new Intent(LoadTournamentActivity.this, StartTournamentActivity.class);
+//			    loadTournament.putExtra(getString(R.string.EXTRA_TOURNAMENT_ID), pos);
+//			    TournamentManager.addTournament(db.getTournament(pos));
+//				startActivity(loadTournament);
+		    }
+		});
+		
 		tListView.setAdapter(adapter);
 	}
-
+	
 	public void loadTournament(View v) {
 		Log.v(TAG, getString(R.string.loadTournament));
-	}
+		
+		
+	}	
 }
