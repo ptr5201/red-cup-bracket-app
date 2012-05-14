4a276a30e7f5156b901cad7029ebb68b2a56f1c9
matthew.j.alioto@gmail.com <matthew.j.alioto@gmail.com@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-05-14 09:40:51 +0000

    LoadTournament screen totally might display something

--- RedCupBracketApp/src/com/redcup/app/activities/LoadTournamentActivity.java
+++ RedCupBracketApp/src/com/redcup/app/activities/LoadTournamentActivity.java
@@ -1,24 +1,37 @@
 package com.redcup.app.activities;
 
-import android.app.Activity;
+import java.util.List;
+
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.redcup.app.R;
+import com.redcup.app.data.RedCupDB;
+import com.redcup.app.model.Tournament;
 
-public class LoadTournamentActivity extends Activity {
+public class LoadTournamentActivity extends ListActivity {
 
 	private static final String TAG = "LoadTournamentActivity";
 	
-	private ListView loadTournamentList;
+	private RedCupDB db;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.loadtournament);
+		db = new RedCupDB(this);
+		db.open();
+		List<String> tournaments = db.getTournaments();
+		db.close();
+		ListView tListView = (ListView)findViewById(android.R.id.list);
 		
-		loadTournamentList = (ListView) findViewById(R.id.loadTournamentListId);
+//		setListAdapter(new ArrayAdapter<String>(this, R.id.tournamentListItemId, tournaments));
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, tournaments);
+
 	}
 
 	public void loadTournament(View v) {
--- RedCupBracketApp/src/com/redcup/app/data/RedCupDB.java
+++ RedCupBracketApp/src/com/redcup/app/data/RedCupDB.java
@@ -1,6 +1,9 @@
 package com.redcup.app.data;
 
-import com.redcup.app.data.Constants;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -9,22 +12,25 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.util.Log;
 
+import com.redcup.app.model.Participant;
+import com.redcup.app.model.Tournament;
+
 
 public class RedCupDB {
 	private SQLiteDatabase db;
 	private final Context context;
 	private final RedCupDBHelper helper;
-	
+
 	public RedCupDB(Context c) {
 		context = c;
 		helper = new RedCupDBHelper(context, 
 				Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION);
 	}
-	
+
 	public void close() {
 		db.close();
 	}
-	
+
 	public void open() {
 		try {
 			db = helper.getWritableDatabase();
@@ -33,7 +39,7 @@ public class RedCupDB {
 			db = helper.getReadableDatabase();
 		}
 	}
-	
+
 	public long insertParticipant(String name) {
 		ContentValues taskValue = new ContentValues();
 		taskValue.put(Constants.Participant.PARTICIPANT_NAME, name);
@@ -42,12 +48,12 @@ public class RedCupDB {
 		taskValue.put(Constants.Participant.DATE_ENDED, 0);
 		return db.insert(Constants.Participant.TABLE_NAME, null, taskValue);
 	}
-	
+
 	public void deleteParticipant(int participantId) {	
 		/*db.delete(Constants.Participant.TABLE_NAME, 
 				Constants.Participant.KEY_ID + "=?", 
 				new String [] { Integer.toString(participantId) });*/
-		
+
 		ContentValues taskValue = new ContentValues();
 		taskValue.put(Constants.Participant.DATE_ENDED, 
 				java.lang.System.currentTimeMillis());
@@ -55,7 +61,7 @@ public class RedCupDB {
 				Constants.Participant.KEY_ID + "=?", 
 				new String [] { Integer.toString(participantId) });
 	}
-	
+
 	public void editParticipant(String newName, int participantId) {
 		ContentValues taskValue = new ContentValues();
 		taskValue.put(Constants.Participant.PARTICIPANT_NAME, newName);
@@ -63,7 +69,7 @@ public class RedCupDB {
 				Constants.Participant.KEY_ID + "=?", 
 				new String [] { Integer.toString(participantId) });
 	}
-	
+
 	public long insertTournament(String name) {
 		ContentValues taskValue = new ContentValues();
 		taskValue.put(Constants.Tournament.TOURNAMENT_NAME, name);
@@ -71,7 +77,7 @@ public class RedCupDB {
 				java.lang.System.currentTimeMillis());
 		return db.insert(Constants.Tournament.TABLE_NAME, null, taskValue);
 	}
-	
+
 	public void startTournament(int tournamentId) {
 		ContentValues taskValue = new ContentValues();
 		taskValue.put(Constants.Tournament.STARTED, 1L);
@@ -79,7 +85,7 @@ public class RedCupDB {
 				Constants.Tournament.KEY_ID + "=?",  
 				new String [] { Integer.toString(tournamentId) });
 	}
-	
+
 	public void completeTournament(int tournamentId) {
 		ContentValues taskValue = new ContentValues();
 		taskValue.put(Constants.Tournament.COMPLETED, 1L);
@@ -87,7 +93,24 @@ public class RedCupDB {
 				Constants.Tournament.KEY_ID + "=?",  
 				new String [] { Integer.toString(tournamentId) });
 	}
-	
+
+	public List<String> getTournaments() {
+		List<String> tournaments = new ArrayList<String>();
+
+		Cursor c = db.query(true, Constants.Tournament.TABLE_NAME, new String[] {Constants.Tournament.TOURNAMENT_NAME}, 
+				null, null, null, null, null, null);
+	        if (c != null) {
+	          c.moveToFirst();
+	        }
+	        while (c.isAfterLast() == false) {
+	            tournaments.add(c.getString(0));
+	            c.moveToNext();
+	        }
+	    c.close();
+		db.close();
+		return tournaments;
+	}
+
 	public long insertTournamentParticipantLink(int tournamentId, int participantId) {
 		ContentValues taskValue = new ContentValues();
 		taskValue.put(Constants.Tournament_Participant.TOURNAMENT_ID, tournamentId);
@@ -96,17 +119,17 @@ public class RedCupDB {
 				java.lang.System.currentTimeMillis());
 		return db.insert(Constants.Tournament_Participant.TABLE_NAME, null, taskValue);
 	}
-	
+
 	public void deleteTournamentParticipantLink(int tournamentId, int participantId) {
 		db.delete(Constants.Tournament_Participant.TABLE_NAME, 
 				Constants.Tournament_Participant.TOURNAMENT_ID + "=?" + " and " + 
 				Constants.Tournament_Participant.PARTICIPANT_ID + "=?", 
 				new String [] { 
-					Integer.toString(tournamentId),
-					Integer.toString(participantId)
-				});
+				Integer.toString(tournamentId),
+				Integer.toString(participantId)
+		});
 	}
-	
+
 	public Cursor getTableCursor(String tableName) {
 		Cursor c = db.query(tableName, null, null, null, null, null, null);
 		return c;
