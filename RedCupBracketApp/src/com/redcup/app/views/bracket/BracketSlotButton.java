c1f29de950b3bdbdaee056f905e45bbaa3f69e6a
jal2633@g.rit.edu <jal2633@g.rit.edu@36b844fc-0c0c-3a8a-c28a-4ce685f6e447>
2012-05-14 01:59:59 +0000

    - Implemented "empty" state for BracketSlotButton - Implemented icon for BracketSlotButton

--- RedCupBracketApp/src/com/redcup/app/views/bracket/BracketSlotButton.java
+++ RedCupBracketApp/src/com/redcup/app/views/bracket/BracketSlotButton.java
@@ -1,10 +1,16 @@
 package com.redcup.app.views.bracket;
 
+import com.redcup.app.R;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -22,13 +28,24 @@ public class BracketSlotButton extends View {
 	private static final int FILL_DEFAULT = Color.argb(255, 255, 255, 255);
 	private static final int FILL_SELECTED = Color.argb(255, 255, 255, 0);
 	private static final int FILL_PRESSED = Color.argb(255, 255, 192, 0);
+	private static final int FILL_EMPTY = Color.argb(255, 160, 160, 160);
 	private static final int FILL_DISABLED = Color.argb(255, 160, 160, 160);
 
-	// Refernce font size
+	// Border colors
+	private static final int BORDER_DEFAULT = Color.BLACK;
+	private static final int BORDER_SELECTED = Color.BLACK;
+	private static final int BORDER_PRESSED = Color.BLACK;
+	private static final int BORDER_EMPTY = Color.BLACK;
+	private static final int BORDER_DISABLED = Color.BLACK;
+
+	// Reference font size
 	private static final int DEFAULT_TEXT_SIZE = 32;
 
 	// Sample text used to compute text sizes
 	private static final String SIZING_SAMPLE_TEXT = "Ty";
+
+	// Button state
+	private boolean isEmpty = false;
 
 	// Border thickness
 	private float borderThickness = 4;
@@ -37,13 +54,17 @@ public class BracketSlotButton extends View {
 	private float cornerRadius = 20;
 
 	// Insets
-	private float insets_top = 20;
+	private float insets_top = 15;
 	private float insets_left = 10;
 	private float insets_bottom = 15;
 	private float insets_right = 10;
+	private float insets_topOffset = 5;
+	private float insets_iconTextSpacing = 5;
 
-	// Diplayed text
+	// Displayed text and icon
 	private String text = null;
+	private Drawable icon = null;
+	private Drawable addIcon;
 
 	// Scale factor
 	private float scale = 1.0f;
@@ -93,32 +114,32 @@ public class BracketSlotButton extends View {
 	 */
 	private void initialize() {
 		this.setClickable(true);
+		this.addIcon = new BitmapDrawable(BitmapFactory.decodeResource(
+				this.getResources(), R.drawable.create_tournament));
 	}
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		boolean result = super.onTouchEvent(event);
+	/**
+	 * Sets the empty state flag on this {@code BracketSlotButton}. If set, this
+	 * flag overrides the appearance of this button but does not affect its
+	 * functionality.
+	 * 
+	 * @param isEmpty
+	 *            the empty state flag on this {@code BracketSlotButton}.
+	 */
+	public void setEmpty(boolean isEmpty) {
+		this.isEmpty = isEmpty;
+		this.invalidate();
+	}
 
-		// Update pressed state
-		if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-			this.setPressed(true);
-			this.invalidate();
-		} else if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-			this.setPressed(false);
-			this.invalidate();
-		} else if (event.getActionMasked() == MotionEvent.ACTION_MOVE) {
-			boolean isPointerOver = false;
-			for (int i = 0; i < event.getPointerCount(); i++) {
-				isPointerOver |= event.getX(i) >= 0
-						&& event.getX(i) <= this.getWidth()
-						&& event.getY(i) >= 0
-						&& event.getY(i) <= this.getHeight();
-			}
-			this.setPressed(isPointerOver);
-			this.invalidate();
-		}
-
-		return result;
+	/**
+	 * Returns the empty state flag on this {@code BracketSlotButton}. If set,
+	 * this flag overrides the appearance of this button but does not affect its
+	 * functionality.
+	 * 
+	 * @return the empty state flag on this {@code BracketSlotButton}.
+	 */
+	public boolean isEmpty() {
+		return this.isEmpty;
 	}
 
 	/**
@@ -214,20 +235,74 @@ public class BracketSlotButton extends View {
 		return this.scale * dimension;
 	}
 
+	public void setIcon(Drawable icon) {
+		this.icon = icon;
+		this.invalidate();
+	}
+
+	public void setIcon(Bitmap icon) {
+		this.icon = new BitmapDrawable(icon);
+		this.invalidate();
+	}
+
+	public void setIcon(int resid) {
+		this.icon = new BitmapDrawable(BitmapFactory.decodeResource(
+				this.getResources(), resid));
+		this.invalidate();
+	}
+
+	public Drawable getIcon() {
+		return this.icon;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		boolean result = super.onTouchEvent(event);
+
+		// Update pressed state
+		if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+			this.setPressed(true);
+			this.invalidate();
+		} else if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+			this.setPressed(false);
+			this.invalidate();
+		} else if (event.getActionMasked() == MotionEvent.ACTION_MOVE) {
+			boolean isPointerOver = false;
+			for (int i = 0; i < event.getPointerCount(); i++) {
+				isPointerOver |= event.getX(i) >= 0
+						&& event.getX(i) <= this.getWidth()
+						&& event.getY(i) >= 0
+						&& event.getY(i) <= this.getHeight();
+			}
+			this.setPressed(isPointerOver);
+			this.invalidate();
+		}
+
+		return result;
+	}
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		Paint paint = new Paint();
 		paint.setAntiAlias(true);
 		paint.setStyle(Style.FILL);
 		int fillColor = FILL_DEFAULT;
+		int borderColor = BORDER_DEFAULT;
 		if (this.isSelected()) {
 			fillColor = FILL_SELECTED;
+			borderColor = BORDER_SELECTED;
+		}
+		if (this.isEmpty()) {
+			fillColor = FILL_EMPTY;
+			borderColor = BORDER_EMPTY;
 		}
 		if (this.isPressed()) {
 			fillColor = FILL_PRESSED;
+			borderColor = BORDER_PRESSED;
 		}
 		if (!this.isEnabled()) {
 			fillColor = FILL_DISABLED;
+			borderColor = BORDER_DISABLED;
 		}
 		int scaledCornerRadius = Math.round(this.applyScale(this.cornerRadius));
 		int scaledTextMarginTop = Math.round(this.applyScale(this.insets_top));
@@ -239,7 +314,7 @@ public class BracketSlotButton extends View {
 				.applyScale(this.insets_bottom));
 
 		// Draw border
-		paint.setColor(Color.BLACK);
+		paint.setColor(borderColor);
 		canvas.drawRoundRect(
 				new RectF(0, 0, this.getWidth(), this.getHeight()),
 				scaledCornerRadius, scaledCornerRadius, paint);
@@ -253,18 +328,38 @@ public class BracketSlotButton extends View {
 				- this.borderThickness, paint);
 
 		// Draw label
-		if (this.text != null) {
+		String text = this.text;
+		Drawable icon = this.icon;
+		if (this.isEmpty()) {
+			text = "Add";
+			paint.setTextSkewX(-0.25f);
+			icon = this.addIcon;
+		}
+		if (icon != null) {
+			float iconScale = (this.getHeight() - scaledTextMarginBottom - scaledTextMarginTop)
+					/ (float) icon.getMinimumHeight();
+			float iconWidth = icon.getMinimumWidth() * iconScale;
+			float iconHeight = icon.getMinimumHeight() * iconScale;
+			icon.setBounds(scaledTextMarginLeft, scaledTextMarginTop,
+					Math.round(scaledTextMarginLeft + iconWidth),
+					Math.round(scaledTextMarginTop + iconHeight));
+			icon.draw(canvas);
+			scaledTextMarginLeft += iconWidth
+					+ this.applyScale(this.insets_iconTextSpacing);
+		}
+		if (text != null) {
 			canvas.save();
 			// Configure paint
 			RectF textAreaBounds = new RectF(scaledTextMarginLeft,
-					scaledTextMarginTop, this.getWidth()
-							- scaledTextMarginRight, this.getHeight()
+					scaledTextMarginTop
+							+ this.applyScale(this.insets_topOffset),
+					this.getWidth() - scaledTextMarginRight, this.getHeight()
 							- scaledTextMarginBottom);
 			Paint textPaint = new Paint(paint);
 			canvas.clipRect(textAreaBounds);
 			textPaint.setColor(Color.BLACK);
 			textPaint.setTextSize(DEFAULT_TEXT_SIZE);
-			textPaint.setUnderlineText(this.isSelected());
+			textPaint.setUnderlineText(this.isSelected() || this.isEmpty());
 
 			// Size text to match height of button
 			Rect measuredBounds = new Rect();
@@ -279,8 +374,8 @@ public class BracketSlotButton extends View {
 					SIZING_SAMPLE_TEXT.length(), measuredBounds);
 
 			// Draw text
-			canvas.drawText(this.text, textAreaBounds.left,
-					textAreaBounds.bottom - measuredBounds.bottom, textPaint);
+			canvas.drawText(text, textAreaBounds.left, textAreaBounds.bottom
+					- measuredBounds.bottom, textPaint);
 			canvas.restore();
 		}
 	}
